57461e8b7d4fb84d0f43785a76c79723bfbe7f2d
kutouten <hiro0402s@yahoo.co.jp>
2025-11-24 23:38:52 +0900

    Initial commit

---
+++ src/main/java/com/github/kutouten/diamondSlot/DiamondSlot.java
@@ -0,0 +1,17 @@
+package com.github.kutouten.diamondSlot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DiamondSlot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
